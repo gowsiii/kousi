5dd08f9df8358a2e217b31ce14d24b1f670bf11a
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-06 22:27:57 +0530

    Create ccc.c

---
+++ ccc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+char a;
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+printf("The given character is vowel");
+else 
+printf("The given character is consonant");
+	return 0;
+}
