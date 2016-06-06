e87a3fae4a8468fab80f6f11881e4b8fd3c89b74
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-06 21:39:02 +0530

    Create aaa.c

---
+++ aaa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n==0)
+	printf("The number is Zero");
+	else if(n>0)
+	printf("The number is positive");
+	else
+	printf("The number is negative");
+	// your code goes here
+	return 0;
+}
