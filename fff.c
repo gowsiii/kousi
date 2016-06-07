d1f12fece9290feea76a62c7673d5c1fa4846340
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-07 16:09:16 +0530

    Create fff.c

---
+++ fff.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	if(n%4==0)
+	printf("The given year is leap year");
+	else
+	printf("The given year is not leap year");
+	// your code goes here
+	return 0;
+}
