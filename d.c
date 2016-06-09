5e4460fc6fb343e129aed6c4bb8bad940c56c910
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-09 15:35:38 +0530

    Create d.c

---
+++ d.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char s[10];
+	scanf("%s",s);
+	if(strcmp(s,"sunday")==0)
+	printf("false");
+	else
+	printf("true");
+	// your code goes here
+	return 0;
+}
