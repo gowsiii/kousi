22eaf1623cfdfd9f712f99e5c8e5e6754d5e7017
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-14 22:10:55 +0530

    Create check.c

---
+++ check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+
+	scanf("%d",&n);
+	if(n>=1&&n<=9)
+	printf("%d",n);
+	else
+	printf("Error! give the value between the  range");
+	// your code goes here
+	return 0;
+}
