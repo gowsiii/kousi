9052940f04b98299b79b04d1655dda4c7b24a127
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-14 22:06:44 +0530

    Create onetonine.c

---
+++ onetonine.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+
+	scanf("%d",&n);
+	if(n>=1&&n<=9)
+	printf("%d",n);
+	else
+	printf("you entered number is not in range");
+	// your code goes here
+	return 0;
+}
