7fcf226200d2fd0e3fb0f588067c4ff6d33f327c
gowsiii <kousalya.ece.2014@gmail.com>
2016-06-14 21:13:00 +0530

    Create summ.c

---
+++ summ.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i;
+	scanf("%d",&n);
+	int a[n],sum=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		sum=sum+a[i];
+	}
+	printf("%d",sum);
+	// your code goes here
+	return 0;
+}
